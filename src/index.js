3b64d35289c13a16280d0bd55eae4ba57e0b8878
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-11-10 15:40:17 +0200

    fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const scores = [
-    { name: "Jim", score: "30" },
-    { name: "Ntare", score: "40" },
+  { name: 'Jim', score: '30' },
+  { name: 'Ntare', score: '40' },
 ];
 
 const scoresContainer = document.getElementById('scores-list');
 
 scores.forEach((score) => {
-    const indivScore = `${score.name}: ${score.score}`;
-    const scoreItem = document.createElement('li');
-    scoreItem.innerHTML = indivScore;
-    scoresContainer.append(scoreItem)
-})
+  const indivScore = `${score.name}: ${score.score}`;
+  const scoreItem = document.createElement('li');
+  scoreItem.innerHTML = indivScore;
+  scoresContainer.append(scoreItem);
+});
